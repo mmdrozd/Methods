e6c765ff2957050763c020b5cc26320baf34cda1
llorracc <llorracc@users.noreply.github.com>
2018-09-10 10:40:51 -0400

    Update Basic_Make-Root-Links.sh

--- Tools/Install/Machines/010_Xubuntu/Scripts/010_Basic_Make-Root-Links.sh
+++ Tools/Install/Machines/010_Xubuntu/Scripts/010_Basic_Make-Root-Links.sh
@@ -10,28 +10,28 @@ cd "$(dirname "$0")" # http://stackoverflow.com/questions/3349105/how-to-set-cur
 [ ! -e /Volumes/Sync/Lib ] && sudo mkdir --mode=a+r /Volumes/Sync/Lib 
 
 if [ ! -e /Volumes/Sync/Lib/config ]; then # if it already exists, this command screws things up so don't do it
-   sudo ln -fs /Methods/Tools/Config/tool /Volumes/Sync/Lib/config
+    sudo ln -fs /Methods/Tools/Config/tool /Volumes/Sync/Lib/config
 fi
 
 if [ -e /media/ro.llorracc.local ]; then # We are working on a home-local machine
     llorraccMethSlvePath=/media/ro.llorracc.local/Volumes/Sync/Dropbox/OthersTo/JHU/Courses/shr/Methods
 
-        llorraccMethMstrPath=/media/rw.llorracc.local/Volumes/Sync/Dropbox/OthersTo/JHU/Courses/pri/Methods
+    llorraccMethMstrPath=/media/rw.llorracc.local/Volumes/Sync/Dropbox/OthersTo/JHU/Courses/pri/Methods
     if [ ! "$(ls -A /media/ro.llorracc.local)" ]; then # we have not mounted the ro.llorracc.local directory
-	    echo 'ro.llorracc.local exists but is empty -- must be mounted to proceed.  Hit return when mounted.'
-	    read answer
+	echo 'ro.llorracc.local exists but is empty -- must be mounted to proceed.  Hit return when mounted.'
+	read answer
     fi
     sudo rm -f /Methods-Slve
     sudo ln -fs $llorraccMethSlvePath /Methods-Slve
     if [ ! "$(ls -A /media/rw.llorracc.local)" ]; then # we have not mounted the rw.llorracc.local directory
-	    echo 'rw.llorracc.local exists but is empty -- must be mounted to proceed.  Hit return when mounted.'
-	    read answer
+	echo 'rw.llorracc.local exists but is empty -- must be mounted to proceed.  Hit return when mounted.'
+	read answer
     fi
     sudo rm -f /Methods-Mstr
     sudo ln -fs $llorraccMethMstrPath /Methods-Mstr
     #    If no /Methods link exists, then create one for /Methods-Mstr; if one does exist, let it remain as is 
     if [ ! -e /Methods ]; then
-       sudo ln -fs /Methods-Mstr /Methods
+	sudo ln -fs /Methods-Mstr /Methods
     fi
 else # it's not a local machine so Methods should be local
     sudo rm -f /Methods # if /Methods exists, the ln syntax below will create /Methods/Methods; delete it to prevent
@@ -53,18 +53,18 @@ fi
 # equivalate /Users (where MacOS stores them) and /home (where Linux stores them)
 
 if [ ! $(uname -s) = "Darwin" ]; then # Not MacOS, assume Linux
-  sudo rm -f /Users
-  sudo ln -fs /home /Users
+    sudo rm -f /Users
+    sudo ln -fs /home /Users
 fi
 
 # Make sure a root password has been set 
 if [ `uname` == 'Linux' ]; then 
-   passwd_root_status=`sudo passwd --status root`
-   arr=($passwd_root_status) # parse the result into individual 'words'
-   if [ arr[1] == 'NP' ]; then # The second 'word' is NP if No Password has been set 
-       echo 'Need to set a root login password.  Do it now.'
-       sudo passwd -i
-   fi
+    passwd_root_status=`sudo passwd --status root`
+    arr=($passwd_root_status) # parse the result into individual 'words'
+    if [ arr[1] == 'NP' ]; then # The second 'word' is NP if No Password has been set 
+	echo 'Need to set a root login password.  Do it now.'
+	sudo passwd -i
+    fi
 fi
 
 # Lengthen the grace period for use of the sudo command
