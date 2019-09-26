4de1bc02fa288ef53679c094a9d51c1fde0ca5bb
llorracc <llorracc@users.noreply.github.com>
2019-09-26 16:47:24 -0400

    Restore download command to start for ISO

--- Tools/Install/Machines/Scripts/Methods-ISO/start_modified-for-methods.sh
+++ Tools/Install/Machines/Scripts/Methods-ISO/start_modified-for-methods.sh
@@ -3,6 +3,18 @@
 
 finishPath=https://raw.githubusercontent.com/ccarrollATjhuecon/Methods/master/Tools/Install/Machines/Scripts/Methods-ISO/finish_modified-for-methods.sh
 
+# define download function
+# courtesy of http://fitnr.com/showing-file-download-progress-using-wget.html
+download()
+{
+    local url=$1
+#    echo -n "    "
+    wget --progress=dot $url 2>&1 | grep --line-buffered "%" | \
+        sed -u -e "s,\.,,g" | awk '{printf("\b\b\b\b%4s", $2)}'
+#    echo -ne "\b\b\b\b"
+#    echo " DONE"
+}
+
 tmp="/tmp"
 
 datetime="$(date +%Y%m%d%H%S)"
