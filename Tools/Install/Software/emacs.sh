635ec76faebdf78381e17af468d86d016254de40
llorracc <llorracc@users.noreply.github.com>
2019-09-29 14:08:12 -0400

    Fix emacs permissions after install

--- Tools/Install/Software/emacs.sh
+++ Tools/Install/Software/emacs.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
-source /Volumes/Sync/Lib/config/bash/dotbashrc-all
+source /Volumes/Sync/Lib/config/bash/dotbashrc-METHODS
 # make machine-wide place for package files
 
 # Set location for downloaded packages which should be available to any user 
 
 sudo apt-get update
+sudo apt-get -y install emacs
+
+$METH_CONFIG/user/make-links-for-users/loop-over-directories-and-make-dot-emacs-link-Linux.sh
+
 sudo mkdir -p /usr/local/share/emacs/site-lisp/elpa/gnupg
 sudo gpg --homedir /usr/local/share/emacs/site-lisp/elpa/gnupg --receive-keys 066DAFCB81E42C40
-
-sudo apt-get -y install emacs
-$METH_CONFIG/user/make-links-for-users/loop-over-directories-and-make-dot-emacs-link-Linux.sh
+sudo chmod -Rf a+rwx /usr/local/share/emacs/site-lisp/elpa/gnupg
