a4fd8215c0bc6303fc646814d479a5c691bd8af3
llorracc <llorracc@users.noreply.github.com>
2019-09-29 01:52:55 -0400

    Fix permissions for Methods-ISO

--- Tools/Config/user/make-links-for-users/loop-over-directories-and-setup-bash-shell-linux.sh
+++ Tools/Config/user/make-links-for-users/loop-over-directories-and-setup-bash-shell-linux.sh
@@ -46,6 +46,8 @@ for d in * ; do
 	#   http://unix.stackexchange.com/questions/257571/why-does-bashrc-check-whether-the-current-shell-is-interactive
 	#   https://www.gnu.org/software/bash/manual/html_node/Bash-Startup-Files.html
 	if [ ! -e /$Users/$d/.ssh/id_rsa.pub ]; then # If there is no public rsa key, make one
+	    sudo mkdir -p /home/methods/.ssh
+	    sudo chmod a+rwx /home/methods/.ssh
 	    ssh-keygen -b 4096 -t rsa -q -N "" -C "methods-auto" -f /home/methods/.ssh/id_rsa
 	fi
 	dateNow=`date '+%Y%m%d-%H%Mh'`
--- Tools/Install/Software/emacs.sh
+++ Tools/Install/Software/emacs.sh
@@ -6,8 +6,8 @@ source /Volumes/Sync/Lib/config/bash/dotbashrc-all
 # Set location for downloaded packages which should be available to any user 
 
 sudo apt-get update
-sudo apt-get -y install emacs
-
-$METH_CONFIG/user/make-links-for-users/loop-over-directories-and-make-dot-emacs-link-Linux.sh
 sudo mkdir -p /usr/local/share/emacs/site-lisp/elpa/gnupg
 sudo gpg --homedir /usr/local/share/emacs/site-lisp/elpa/gnupg --receive-keys 066DAFCB81E42C40
+
+sudo apt-get -y install emacs
+$METH_CONFIG/user/make-links-for-users/loop-over-directories-and-make-dot-emacs-link-Linux.sh
