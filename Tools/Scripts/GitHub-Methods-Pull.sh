9f8f226550f58e86f65d236b49fef88b88a5b0dd
llorracc <llorracc@users.noreply.github.com>
2019-10-04 14:56:11 -0400

    Only rerun Basic if upstream has changed

--- Tools/Scripts/GitHub-Methods-Pull.sh
+++ Tools/Scripts/GitHub-Methods-Pull.sh
@@ -1,9 +1,28 @@
 #!/bin/bash
 
-cd ~/GitHub/ccarrollATjhuecon/Methods
+if [ -e ~/GitHub/ccarrollATjhuecon/Methods ]; then
+    cd ~/GitHub/ccarrollATjhuecon/Methods
+    git checkout master # Make sure on master branch
+else
+    echo 'No ~/GitHub/ccarrollATjhuecon/Methods; aborting'
+    exit
+fi
+
 if curl --output /dev/null --head --silent --head --fail "https://github.com/ccarrollATjhuecon/Methods" ; then # We have access
-    git fetch
-    git reset --hard
-    git pull
+# https://stackoverflow.com/questions/3258243/check-if-pull-needed-in-git
+    UPSTREAM=${1:-'@{u}'}
+    LOCAL=$(git rev-parse @)
+    REMOTE=$(git rev-parse "$UPSTREAM")
+    BASE=$(git merge-base @ "$UPSTREAM")
+
+    if [ $LOCAL = $REMOTE ]; then
+	echo "No changes in upstream Methods repo; continuing"
+    else
+	git fetch
+	git reset --hard
+	git pull
+        echo 'Rerunning Basic setup script (in case it has changed)'
+        /Methods/Tools/Install/Machines/010_Xubuntu/Scripts/010_Basic.sh
+    fi
 fi
 
--- Tools/Scripts/GitHub-Methods-Update.sh
+++ Tools/Scripts/GitHub-Methods-Update.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-echo '' ; echo 'Switching automatically to existing ~/GitHub/ccarrollATjhuecon/Methods folder. '
-sudo rm -f /Methods ; sudo ln -fs /home/methods/GitHub/ccarrollATjhuecon/Methods /Methods
-echo 'Pulling latest courseware from GitHub'
-/Methods/Tools/Scripts/GitHub-Methods-Pull.sh
-echo 'Rerunning Basic setup script (in case it has changed)'
-/Methods/Tools/Install/Machines/010_Xubuntu/Scripts/010_Basic.sh
+# 20191004- Functionality merged into GitHub-Methods-Pull.sh
+
+# echo '' ; echo 'Switching automatically to existing ~/GitHub/ccarrollATjhuecon/Methods folder. '
+# sudo rm -f /Methods ; sudo ln -fs /home/methods/GitHub/ccarrollATjhuecon/Methods /Methods
+# echo 'Pulling latest courseware from GitHub'
+# /Methods/Tools/Scripts/GitHub-Methods-Pull.sh
+# echo 'Rerunning Basic setup script (in case it has changed)'
+# /Methods/Tools/Install/Machines/010_Xubuntu/Scripts/010_Basic.sh
