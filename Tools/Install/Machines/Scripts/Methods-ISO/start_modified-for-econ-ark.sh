702e9ce59c9978b814d4cc5a67b349a8ffa4af0c
llorracc <@users.noreply.github.com>
2020-01-12 08:09:56 -0500

    econ-ark ISO make fix

--- Tools/Install/Machines/Scripts/Methods-ISO/start_modified-for-econ-ark.sh
+++ Tools/Install/Machines/Scripts/Methods-ISO/start_modified-for-econ-ark.sh
@@ -1,4 +1,52 @@
 #!/bin/bash
+#!/bin/bash
+# Adapted from http://askubuntu.com/questions/505919/how-to-install-anaconda-on-ubuntu
+
+sudo ls  # make sure we have root permission from the outset
+scriptDir="$(dirname "`realpath $0`")"
+
+if [ -e /tmp/Anaconda ]; then # delete any prior install
+    sudo rm -Rf /tmp/Anaconda
+fi
+
+mkdir /tmp/Anaconda ; cd /tmp/Anaconda
+
+CONTREPO=https://repo.continuum.io/archive/
+# Stepwise filtering of the html at $CONTREPO
+# Get the topmost line that matches our requirements, extract the file name.
+ANACONDAURL=$(wget -q -O - $CONTREPO index.html | grep "Anaconda3-" | grep "Linux" | grep "86_64" | head -n 1 | cut -d \" -f 2)
+cmd="wget -O /tmp/Anaconda/$ANACONDAURL $CONTREPO$ANACONDAURL ; cd /tmp/Anaconda"
+echo "$cmd"
+eval "$cmd"
+
+cmd="sudo rm -Rf /usr/local/anaconda3 ; chmod a+x /tmp/Anaconda/$ANACONDAURL ; /tmp/Anaconda/$ANACONDAURL -b -p /usr/local/anaconda3"
+echo "$cmd"
+eval "$cmd"
+
+addToPath='export PATH=/usr/local/anaconda3/bin:$PATH'
+eval "$addToPath"
+sudo chmod u+w /etc/environment
+sudo sed -e 's\/usr/local/sbin:\/usr/local/anaconda3/bin:/usr/local/sbin:\g' /etc/environment > /tmp/environment
+sudo sed -e 's\/usr/local/anaconda3/bin:/usr/local/anaconda3/bin\/usr/local/anaconda3/bin\g' /tmp/environment > /tmp/environment2 # eliminate any duplicates
+sudo mv /tmp/environment2 /etc/environment # Weird permissions issue prevents direct redirect into /etc/environment
+sudo chmod u-w /etc/environment
+
+if [ ! -e /etc/sudoers.d/anaconda3 ]; then # Modify secure path so that anaconda commands will work with sudo
+    sudo mkdir -p /etc/sudoers.d
+    sudo echo 'Defaults secure_path="/usr/local/anaconda3/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/snap/bin:/bin"' | sudo tee /etc/sudoers.d/anaconda3
+fi
+
+echo "$PATH"
+source /etc/environment
+
+conda update --yes conda
+conda update --yes anaconda
+
+conda install --yes -c anaconda scipy
+conda install --yes -c anaconda pyopengl # Otherwise you get an error "Segmentation fault (core dumped)"
+
+$scriptDir/Anaconda-jupyter_contrib_nbextensions.sh
+#/Methods/Tools/Config/tool/jupytext/default.sh
 sudo apt -y install bash-completion xsel git curl wget cifs-utils openssh-server nautilus-share xclip
 #!/bin/bash
 
