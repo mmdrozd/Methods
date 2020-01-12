b5d15e3784bf5af4e78bc4d9c33689835e184572
llorracc <@users.noreply.github.com>
2020-01-11 22:45:38 -0500

    Add emacs etc

--- Tools/Install/Machines/Scripts/Methods-ISO/start_modified-for-econ-ark_make.sh
+++ Tools/Install/Machines/Scripts/Methods-ISO/start_modified-for-econ-ark_make.sh
@@ -3,9 +3,12 @@ scriptDir="$(dirname "`realpath $0`")"
 # scriptDir=/Volumes/Data/GitHub/ccarrollATjhuecon/Methods/Tools/Install/Machines/Scripts/Methods-ISO/
 cd "$scriptDir"
 start='start_modified-for-econ-ark_start.sh'
+touch "$start"
 sudo chmod a+w "$start"
-echo -n '' > "$start"
+
+
 cat ~/GitHub/ccarrollATjhuecon/Methods/Tools/Install/Languages/Anaconda3-Latest.sh          | tee "$start"
+echo 'sudo apt -y install bash-completion xsel git curl wget cifs-utils openssh-server nautilus-share xclip' >> "$start"
 cat ~/GitHub/ccarrollATjhuecon/Methods/Tools/Install/Toolkits/ARK.sh                        | tee "$start"
 cat ~/GitHub/ccarrollATjhuecon/Methods/Tools/Install/Packages/VirtualBox-Guest-Additions.sh | tee "$start"
 chmod a+x "$start"
