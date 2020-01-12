a4a029b6c4b0a809aa56686fa4f3eed3df311526
llorracc <@users.noreply.github.com>
2020-01-11 23:10:43 -0500

    Further fixes for econ-ark ISO maker

--- Tools/Install/Machines/Scripts/Methods-ISO/start_modified-for-econ-ark_make.sh
+++ Tools/Install/Machines/Scripts/Methods-ISO/start_modified-for-econ-ark_make.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
 scriptDir="$(dirname "`realpath $0`")"
+cd "$scriptDir"
 echo ''
 echo "scriptDir=$scriptDir"
 echo ''
 # scriptDir=/Volumes/Data/GitHub/ccarrollATjhuecon/Methods/Tools/Install/Machines/Scripts/Methods-ISO/
 cd "$scriptDir"
-start='start_modified-for-econ-ark_start.sh'
+start='start_modified-for-econ-ark.sh'
+rm "$start"
 touch "$start"
-sudo chmod a+w "$start"
+sudo chmod a+xw "$start"
 
 echo '#!/bin/bash' > "$start"
 
-#cat ~/GitHub/ccarrollATjhuecon/Methods/Tools/Install/Languages/Anaconda3-Latest.sh          | tee "$start"
+cat ~/GitHub/ccarrollATjhuecon/Methods/Tools/Install/Languages/Anaconda3-Latest.sh         >> tee "$start"
 echo 'sudo apt -y install bash-completion xsel git curl wget cifs-utils openssh-server nautilus-share xclip' >> "$start"
-cat ~/GitHub/ccarrollATjhuecon/Methods/Tools/Install/Toolkits/ARK.sh                        | tee "$start"
-cat ~/GitHub/ccarrollATjhuecon/Methods/Tools/Install/Packages/VirtualBox-Guest-Additions.sh | tee "$start"
+cat ~/GitHub/ccarrollATjhuecon/Methods/Tools/Install/Toolkits/ARK.sh                        >> "$start"
+cat ~/GitHub/ccarrollATjhuecon/Methods/Tools/Install/Packages/VirtualBox-Guest-Additions.sh >> "$start"
 chmod a+x "$start"
--- Tools/Install/Machines/Scripts/Methods-ISO/start_modified-for-econ-ark_start.sh
+++ Tools/Install/Machines/Scripts/Methods-ISO/start_modified-for-econ-ark_start.sh
@@ -1,5 +1,26 @@
 #!/bin/bash
+# This script will be in the /var/local/methods directory and should be executed by root at the first boot 
 
-# https://askubuntu.com/questions/499070/install-virtualbox-guest-addition-terminal
+finishPath=https://raw.githubusercontent.com/ccarrollATjhuecon/Methods/master/Tools/Install/Machines/Scripts/Methods-ISO/finish_modified-for-methods.sh
 
-sudo apt -y install build-essential module-assistant virtualbox-guest-dkms virtualbox-guest-utils virtualbox-guest-x11
+# set defaults
+default_hostname="$(hostname)"
+default_domain="jhu.edu"
+
+# define download function
+# courtesy of http://fitnr.com/showing-file-download-progress-using-wget.html
+download()
+{
+    local url=$1
+#    echo -n "    "
+    wget --progress=dot $url 2>&1 | grep --line-buffered "%" | \
+        sed -u -e "s,\.,,g" | awk '{printf("\b\b\b\b%4s", $2)}'
+#    echo -ne "\b\b\b\b"
+#    echo " DONE"
+}
+
+tmp="/tmp"
+
+datetime="$(date +%Y%m%d%H%S)"
+sed -i "s/ubuntu/Xub-$datetime/g" /etc/hostname
+sed -i "s/ubuntu/Xub-$datetime/g" /etc/hosts
