f328c9297bab46b087d5ba278298ff250a26cb4f
llorracc <@users.noreply.github.com>
2020-01-11 22:38:30 -0500

    Add VB Guest Additions

--- Tools/Install/Machines/Scripts/Methods-ISO/start_modified-for-econ-ark_start.sh
+++ Tools/Install/Machines/Scripts/Methods-ISO/start_modified-for-econ-ark_start.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-#Download and extract HARK, REMARK, DemARK from GitHUB repository
+# https://askubuntu.com/questions/499070/install-virtualbox-guest-addition-terminal
 
-conda install --yes -c conda-forge econ-ark
-mkdir -p ~/GitHub/econ-ark
-cd ~/GitHub/econ-ark
-git clone https://github.com/econ-ark/REMARK.git
-git clone https://github.com/econ-ark/DemARK.git
+sudo apt -y install build-essential module-assistant virtualbox-guest-dkms virtualbox-guest-utils virtualbox-guest-x11
