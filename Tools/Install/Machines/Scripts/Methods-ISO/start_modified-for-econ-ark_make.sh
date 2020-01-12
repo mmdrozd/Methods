87fd521df464b00696b03881d4fd6f0fad5a3208
llorracc <@users.noreply.github.com>
2020-01-11 22:52:12 -0500

    Ignore anaconda3 temporarily

--- Tools/Install/Machines/Scripts/Methods-ISO/start_modified-for-econ-ark_make.sh
+++ Tools/Install/Machines/Scripts/Methods-ISO/start_modified-for-econ-ark_make.sh
@@ -6,8 +6,9 @@ start='start_modified-for-econ-ark_start.sh'
 touch "$start"
 sudo chmod a+w "$start"
 
+echo '#!/bin/bash' > "$start"
 
-cat ~/GitHub/ccarrollATjhuecon/Methods/Tools/Install/Languages/Anaconda3-Latest.sh          | tee "$start"
+#cat ~/GitHub/ccarrollATjhuecon/Methods/Tools/Install/Languages/Anaconda3-Latest.sh          | tee "$start"
 echo 'sudo apt -y install bash-completion xsel git curl wget cifs-utils openssh-server nautilus-share xclip' >> "$start"
 cat ~/GitHub/ccarrollATjhuecon/Methods/Tools/Install/Toolkits/ARK.sh                        | tee "$start"
 cat ~/GitHub/ccarrollATjhuecon/Methods/Tools/Install/Packages/VirtualBox-Guest-Additions.sh | tee "$start"
