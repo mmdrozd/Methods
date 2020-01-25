d9c8479bcd16091d3f71c69652bbcc516ce8dd3e
llorracc <@users.noreply.github.com>
2020-01-25 17:52:14 +0100

    Add nbextesions and other requirements

--- Tools/Install/Machines/Scripts/Methods-ISO/make_modified-for-econ-ark.sh
+++ Tools/Install/Machines/Scripts/Methods-ISO/make_modified-for-econ-ark.sh
@@ -8,7 +8,7 @@ myuser="econ-ark"
 sudo "./start_modified-for-$myuser-make.sh"
 sudo "./finish_modified-for-$myuser-make.sh"
 
-econarktools="/home/econ-ark/GitHub/econ-ark/econ-ark-tools/"
+econarktools="~/GitHub/econ-ark/econ-ark-tools/"
 ISOmaker="Virtual/Machine/VirtualBox/ISO-maker"
 cp -a "$scriptDir/start_modified-for-$myuser.sh"  "$econarktools/$ISOmaker/start.sh"
 echo cp -a "$scriptDir/start_modified-for-$myuser.sh"  "$econarktools/$ISOmaker/start.sh"
@@ -21,7 +21,7 @@ mkdir -p  "$econarktools/$ISOmaker/root"
 rsync -avr "$scriptDir/root/" "$econarktools/$ISOmaker/root"
 cp -a "$scriptDir/../Methods-ISO-make/ubuntu-desktop-unattended-installation/create-unattended-iso_Econ-ARK.sh" "$econarktools/$ISOmaker"
 cd "$econarktools/$ISOmaker"
-rpl -Rf ".sh" ".sh" *
+rpl -Rf "_modified-for-econ-ark.sh" ".sh" *
 
-rpl -f 'https://raw.githubusercontent.com/econ-ark/econ-ark-tools/master/Virtual/Machine/VirtualBox/ISO-maker' "https://raw.githubusercontent.com/econ-ark/econ-ark-tools/master/$ISOmaker" *
-rpl -vf 'iso_done="/usr/local/share/data/drive.google.com/econ-ark@jhuecon.org/Resources/Virtual/Machine"' 'iso_done="/usr/local/share/data/drive.google.com/econ-ark@jhuecon.org/Resources/Virtual/Machine"'  * 
+rpl -f 'https://raw.githubusercontent.com/ccarrollATjhuecon/Methods/master/Tools/Install/Machines/Scripts/Methods-ISO' "https://raw.githubusercontent.com/econ-ark/econ-ark-tools/master/$ISOmaker" *
+rpl -vf 'iso_done="/media/sf_VirtualBox"' 'iso_done="/usr/local/share/data/drive.google.com/econ-ark@jhuecon.org/Resources/Virtual/Machine"'  * 
