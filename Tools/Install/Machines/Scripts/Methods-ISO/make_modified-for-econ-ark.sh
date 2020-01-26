362842f96187e002cd3fd14047831cf14b345ce6
ccarrollATjhuecon <ccarroll@jhuecon.org>
2020-01-26 12:37:29 +0100

    ISO do not use ~ in pathnames

--- Tools/Install/Machines/Scripts/Methods-ISO/make_modified-for-econ-ark.sh
+++ Tools/Install/Machines/Scripts/Methods-ISO/make_modified-for-econ-ark.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 scriptDir="$(dirname "`realpath $0`")"
+
+# scriptDir=/home/econ-ark/GitHub/ccarrollATjhuecon/Methods/Tools/Install/Machines/Scripts/Methods-ISO
 cd "$scriptDir"
 
 myuser="econ-ark"
@@ -8,18 +10,21 @@ myuser="econ-ark"
 sudo "./start_modified-for-$myuser-make.sh"
 sudo "./finish_modified-for-$myuser-make.sh"
 
-econarktools="~/GitHub/econ-ark/econ-ark-tools/"
+econarktools="/home/$myuser/GitHub/econ-ark/econ-ark-tools"
 ISOmaker="Virtual/Machine/VirtualBox/ISO-maker"
-cp -a "$scriptDir/start_modified-for-$myuser.sh"  "$econarktools/$ISOmaker/start.sh"
-echo cp -a "$scriptDir/start_modified-for-$myuser.sh"  "$econarktools/$ISOmaker/start.sh"
-cp -a "$scriptDir/finish_modified-for-$myuser.sh" "$econarktools/$ISOmaker/finish.sh"
+cp -af "$scriptDir/start_modified-for-$myuser.sh"  "$econarktools/$ISOmaker/start.sh"
+echo cp -af "$scriptDir/start_modified-for-$myuser.sh"  "$econarktools/$ISOmaker/start.sh"
 
-cp -a "ks.cfg" "$econarktools/$ISOmaker"
-cp -a "rc.local" "$econarktools/$ISOmaker"
-cp -a "econ-ark.seed" "$econarktools/$ISOmaker"
+cp -af "$scriptDir/finish_modified-for-$myuser.sh" "$econarktools/$ISOmaker/finish.sh"
+echo cp -af "$scriptDir/finish_modified-for-$myuser.sh" "$econarktools/$ISOmaker/finish.sh"
+exit
+
+cp -af "ks.cfg" "$econarktools/$ISOmaker"
+cp -af "rc.local" "$econarktools/$ISOmaker"
+cp -af "econ-afrk.seed" "$econarktools/$ISOmaker"
 mkdir -p  "$econarktools/$ISOmaker/root"
-rsync -avr "$scriptDir/root/" "$econarktools/$ISOmaker/root"
-cp -a "$scriptDir/../Methods-ISO-make/ubuntu-desktop-unattended-installation/create-unattended-iso_Econ-ARK.sh" "$econarktools/$ISOmaker"
+rsync -afvr "$scriptDir/root/" "$econarktools/$ISOmaker/root"
+cp -af "$scriptDir/../Methods-ISO-make/ubuntu-desktop-unattended-installation/create-unattended-iso_Econ-ARK.sh" "$econarktools/$ISOmaker"
 cd "$econarktools/$ISOmaker"
 rpl -Rf "_modified-for-econ-ark.sh" ".sh" *
 
