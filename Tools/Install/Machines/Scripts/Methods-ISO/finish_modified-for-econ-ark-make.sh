2f430d5c61972c78b6ca90fcd33fa359ff45395d
ccarrollATjhuecon <ccarroll@jhuecon.org>
2020-01-26 14:06:51 +0100

    ISO fix mkdir GitHub

--- Tools/Install/Machines/Scripts/Methods-ISO/finish_modified-for-econ-ark-make.sh
+++ Tools/Install/Machines/Scripts/Methods-ISO/finish_modified-for-econ-ark-make.sh
@@ -22,10 +22,11 @@ echo "gpg --homedir /home/$myuser/.emacs.d/elpa/gnupg --receive-keys 066DAFCB81E
 cat ~/GitHub/ccarrollATjhuecon/Methods/Tools/Install/Toolkits/ARK.sh                        | fgrep -v "!/bin/bash"  >> "$finish"
 chown "$myuser:$myuser" /home/$myuser/GitHub
 cat ~/GitHub/econ-ark/REMARK/binder/postBuild | fgrep -v "#!/bin/bash" >> "$finish"
-echo 'cd ~/GitHub/econ-ark/REMARK/binder ; pip3 install -r requirements.txt' >> "$finish" 
+echo 'cd /usr/local/share/data/GitHub/econ-ark/REMARK/binder ; pip install -r requirements.txt' >> "$finish" 
 
 cat ~/GitHub/ccarrollATjhuecon/Methods/Tools/Install/Packages/VirtualBox-Guest-Additions.sh | fgrep -v "!/bin/bash" >> "$finish"
-echo "mkdir -p /home/$myuser/GitHub/econ-ark ; ln -s /usr/local/share/GitHub/econ-ark /home/$myuser/GitHub/econ-ark" >> "$finish" 
+echo "mkdir -p /home/$myuser/GitHub ; ln -s /usr/local/share/GitHub/econ-ark /home/$myuser/GitHub/econ-ark" >> "$finish" 
+echo "chown $myuser:$myuser /home/$myuser/GitHub" >> "$finish" 
 echo "chown -Rf $myuser:$myuser /usr/local/share/GitHub/econ-ark # Make it be owned by econ-ark user " >> "$finish" 
 
 echo ''                                               >> "$finish" 
