400c9eb696e8958a95b2572f3edcc66763c288aa
ccarrollATjhuecon <ccarroll@jhuecon.org>
2020-01-26 21:33:25 +0100

    ISO update finish

--- Tools/Install/Machines/Scripts/Methods-ISO/finish_modified-for-econ-ark.sh
+++ Tools/Install/Machines/Scripts/Methods-ISO/finish_modified-for-econ-ark.sh
@@ -70,6 +70,7 @@ cd "$arkHome"
 git clone https://github.com/econ-ark/REMARK.git
 git clone https://github.com/econ-ark/HARK.git
 git clone https://github.com/econ-ark/DemARK.git
+chmod a+rw -Rf /usr/local/share/data/GitHub/econ-ark
 
 echo 'This is your local, personal copy of HARK; it is also installed systemwide.  '    >  HARK-README.md
 echo 'Local mods will not affect systemwide, unless you change the default source via:' >> HARK-README.md
@@ -80,21 +81,6 @@ echo '(You can switch back to the systemwide version using pip install econ-ark)
 
 echo 'This is your local, personal copy of DemARK, which you can modify.  '    >  DemARK-README.md
 echo 'This is your local, personal copy of REMARK, which you can modify.  '    >  REMARK-README.md
-
-#!/bin/sh
-pip install jupyter_contrib_nbextensions
-jupyter contrib nbextension install --user
-jupyter nbextension enable codefolding/main
-jupyter nbextension enable codefolding/edit
-jupyter nbextension enable toc2/main
-jupyter nbextension enable collapsible_headings/main
-python -m cite2c.install
-result=$(python <<EOF
-from notebook.services.config.manager import ConfigManager
-cm = ConfigManager()
-cm.update('cite2c', {'zotero':{"user_id": "5043554","username": "econ-ark","access_token": "XZpH9NsoAZmDMmjLKiy8xMXX"}})
-EOF
-      )
 
 cd /usr/local/share/data/GitHub/econ-ark/REMARK/binder ; pip install -r requirements.txt
 
